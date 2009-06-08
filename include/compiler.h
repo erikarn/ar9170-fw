656660afe4289c529ca86308a2002dc7a795fa75
Johannes Berg <johannes@sipsolutions.net>
2009-06-08 22:44:25 +0200

    add ARRAY_SIZE

--- include/compiler.h
+++ include/compiler.h
@@ -6,8 +6,13 @@
 #define __packed	__attribute__((packed))
 
 #define BUILD_BUG_ON(condition)	((void)sizeof(char[1 - 2*!!(condition)]))
+#define BUILD_BUG_ON_ZERO(e) (sizeof(char[1 - 2 * !!(e)]) - 1)
 
 #define ALIGN(x,a)		__ALIGN_MASK(x,(typeof(x))(a)-1)
 #define __ALIGN_MASK(x,mask)	(((x)+(mask))&~(mask))
+
+#define __must_be_array(a) \
+  BUILD_BUG_ON_ZERO(__builtin_types_compatible_p(typeof(a), typeof(&a[0])))
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]) + __must_be_array(arr))
 
 #endif				/* __COMPILER */
